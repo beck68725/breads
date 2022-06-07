b2e7b3be0ac17b73dd75bb15b984c620d8de81af
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-07 18:58:10 -0500

    added breads_controllers.js

--- breads/server.js
+++ breads/server.js
@@ -15,3 +15,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
 })
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
