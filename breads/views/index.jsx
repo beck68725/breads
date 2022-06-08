2efc01e452f30e9b299821959baf89cafe3bebb7
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-07 20:28:51 -0500

    code along bonus

--- breads/views/index.jsx
+++ breads/views/index.jsx
@@ -3,25 +3,25 @@ const Default = require('./layouts/Default')
 
 function Index ({breads}) {
     return (
-        <Default>
-    <h2>Index Page</h2>
-    {/* <p>I have {breads[0].name} bread!</p> */}
-    {/* This is a JSX comment. */}
-    <ul>
-        {
-        breads.map((bread, index)=> {
-            return (
-            <li key={index}>
-                <a href={`/breads/${index}`}>
-                {bread.name}
-                </a>
-            </li>
-            )
-        })
-        }
+        <Default title={title}>
+            <h2>Index Page</h2>
+            {/* <p>I have {breads[0].name} bread!</p> */}
+            {/* This is a JSX comment. */}
+            <ul>
+                {
+                breads.map((bread, index)=> {
+                    return (
+                    <li key={index}>
+                        <a href={`/breads/${index}`}>
+                        {bread.name}
+                        </a>
+                    </li>
+                    )
+                })
+                }
 
-    </ul>
-    </Default>
+            </ul>
+        </Default>
 
       
     )
