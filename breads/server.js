49f3ceafb6f21b3c3fd2113c9252b470eb0873ee
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-07 19:15:23 -0500

    sending index and show data

--- breads/server.js
+++ breads/server.js
@@ -8,20 +8,15 @@ const app = express()
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+    res.send('Welcome to an Awesome App about Breads')
 })
+  
+// Breads
+const breadsController = require('./controllers/breads_controllers.js')
+app.use('/breads', breadsController)
+  
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
 })
-
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
