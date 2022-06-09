ce57e2dd0309e75382a603eaecd694b447ae08b4
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-09 18:58:21 -0500

    code along 1 mod 4

--- breads/controllers/breads_controllers.js
+++ breads/controllers/breads_controllers.js
@@ -12,11 +12,15 @@ breads.get('/', (req, res) => {
     )
 })
 
-
-
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
-  
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.render('show404')
+  }
+})
+
 module.exports = breads
---
+++ breads/views/show404.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function show404 () {
+      return (
+        <Default>
+            <h3>this is my 404</h3>
+        </Default>
+
+      )
+  }
+  
+
+module.exports = show404
