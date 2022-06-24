0d0c88e1a49514b1e066a01e23ae7a8260b2e194
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-23 19:19:57 -0500

    added a bakers model

--- breads/server.js
+++ breads/server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -15,6 +17,9 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -24,6 +29,10 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controllers.js')
 app.use('/breads', breadsController)
+
+//bakers
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
 
 // 404 Page
 app.get('*', (req, res) => {
